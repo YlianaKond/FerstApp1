b1543998571e3b26f50f5681fcf3e0a7b5d8854c
2839-23 <klimkina53@gmail.com>
2026-01-31 11:26:51 +0300

    task

--- app/src/main/java/com/example/KondysovaKlinrina/MainActivity.kt
+++ app/src/main/java/com/example/KondysovaKlinrina/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
-import android.widget.Button
-import android.widget.TextView
 import androidx.core.view.WindowInsetsCompat
 import com.example.KondysovaKlinrina.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
@@ -16,18 +14,16 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-            val button: Button = findViewById(R.id.button)
-            val board: TextView = findViewById(R.id.result)
 
-            button.setOnClickListener {
+
+            binding.button.setOnClickListener {
                 val number = (0..6).random()
-                board.text = number.toString()
+                binding.result.text = number.toString()
             }
         }
     }
